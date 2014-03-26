23be44866e2a77a5435836a6ded8b2981a1efb67
Adrian Arnautu <arnautu.adrian@gmail.com>
2014-03-26 21:40:32 +0200

    minor fixes

--- c/max_number.c
+++ c/max_number.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-void main()
-{
-float a, b, max;   //declar doua nr,si max, am pus float pentru ca nu e specificat ce fel de numere sunt.
+int main() {
+	//declar doua nr,si max, am pus float pentru ca nu e specificat ce fel de numere sunt.
+	float a, b, max;
 
-printf("a="); scanf("%f", &a);  //citesc numerele 
-printf("b="); scanf("%f", &b);
+	// citesc numerele
+	printf("a = "); scanf("%f", &a);
+	printf("b = "); scanf("%f", &b);
 
-max = a;               // max=a, daca b este mai mare ca max, atunci max egal b
-if (b>max)
-max = b;
+	// max=a, daca b este mai mare ca max, atunci max egal b
+	max = a;
 
-printf("Cel mai mare numar este %5.1f", max);  //afisarea rezultatului
+	if (b > max) {
+		max = b;
+	}
 
+	printf("Cel mai mare numar este %5.1f", max);  //afisarea rezultatului
+
+	return 0;
 }
